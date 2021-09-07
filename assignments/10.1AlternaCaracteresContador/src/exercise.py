b6ebece2a6bcb0be4f7374540760e57f8a6be6e4
SofiaAlvarez01 <89089343+SofiaAlvarez01@users.noreply.github.com>
2021-09-07 11:15:31 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
+    n= int(input())
+    a=0
+    c= "#"
+    while a<n:
+        print (c)
+        if c== "#":
+            c= "%"
+        elif c== "%":
+            c= "#"
+        a=a+1
     pass
 
 if __name__=='__main__':   
