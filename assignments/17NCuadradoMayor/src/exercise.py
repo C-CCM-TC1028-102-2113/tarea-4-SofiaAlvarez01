ba0d37626ae9c4a2de99eea29437dc1562d7722d
SofiaAlvarez01 <89089343+SofiaAlvarez01@users.noreply.github.com>
2021-09-07 13:35:04 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,13 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    a=1
+    n= int(input())
+    c=0
+    while c<= n:
+        a=a+1
+        c= a*a
+    print (a)
     pass
 
 if __name__=='__main__':
